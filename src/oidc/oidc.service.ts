96ca4444fbf01cc8d4b4a348f7fad1122ab1d3e2
Joseph Page <josephpage@users.noreply.github.com>
2024-03-18 18:14:20 +0100

    working demo

--- src/oidc/oidc.service.ts
+++ src/oidc/oidc.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Req, Res } from '@nestjs/common';
 
 import {
   OIDC_PROVIDER_MODULE,
@@ -15,10 +15,55 @@ export class OidcService {
   ) {
     this.oidc = new opm.Provider('http://localhost:3000', {
       // the rest of your configuration...
+
+      clients: [
+        {
+          client_id: 'foo',
+          client_secret: 'bar',
+          redirect_uris: ['http://localhost:3000/cb'],
+          grant_types: ['authorization_code'], //  'implicit'
+          response_types: ['code'], // 'code id_token'
+        },
+      ],
+      clientDefaults: {
+        grant_types: ['authorization_code'],
+        id_token_signed_response_alg: 'RS256',
+        response_types: ['code'],
+        token_endpoint_auth_method: 'client_secret_basic',
+      },
+      pkce: {
+        required: () => false,
+      },
+      cookies: {
+        keys: ['my-secret-key'],
+      },
+      features: {
+        devInteractions: { enabled: true }, // change this to false to disable the dev interactions
+        userinfo: { enabled: true },
+      },
+      interactions: {
+        url(_ /* ctx */, interaction) {
+          return `/interaction/${interaction.uid}`;
+        },
+      },
     });
   }
 
+  // Below are the methods that you can use to interact with the oidc-provider library
+
   callback: Provider['callback'] = () => {
     return this.oidc.callback();
+  };
+
+  interactionDetails: Provider['interactionDetails'] = (req, res) => {
+    return this.oidc.interactionDetails(req, res);
+  };
+
+  interactionFinished: Provider['interactionFinished'] = (req, res, result) => {
+    return this.oidc.interactionFinished(req, res, result);
+  };
+
+  interactionResult: Provider['interactionResult'] = (req, res, result) => {
+    return this.oidc.interactionResult(req, res, result);
   };
 }
